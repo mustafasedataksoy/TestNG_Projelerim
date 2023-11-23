ebcdb75e31c4e308ea26b68bd76e34ba8cc69808
musse <mus.sedat.aksoy@gmail.com>
2023-11-23 12:30:47 +0300

    Priority sırasılaması

--- src/Gun01/Giris.java
+++ src/Gun01/Giris.java
@@ -1,2 +1,26 @@
-package Gun01;public class Giris {
+package Gun01;
+
+import org.testng.annotations.Test;
+
+public class Giris {
+
+    @Test(priority = 1)
+     void webSitesiiniAc()
+    {
+        System.out.println("driver tanımlandı ve web sitesi açıldı");
+    }
+
+    @Test(priority = 2)
+    void loginİsleminiYap()
+    {
+        System.out.println("login test işlemlerii yapıldı");
+    }
+
+    @Test(priority = 3)
+    void driverKapat()
+    {
+        System.out.println("driver kapatıldı ve çıkıldı");
+    }
+
+
 }
